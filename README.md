9dcda2190839cc5a3cd0d23cd28da8a14ad36405
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-04-13 22:38:59 +1200

    Implemented new changes and deployed

--- README.md
+++ README.md
@@ -1,35 +1,50 @@
-# IPFS Upload - Using Infura API
+# Estuary
 
-- This is a simple React App using which you can upload any file to IPFS and it will generate an IPFS hash and a link which can be then shared with people or used however you want.
-- Since, Infura discontinued its free IPFS service you will need an API key to use it.
+- This is a simple React App using which you can upload any file to IPFS and it will generate an IPFS hash, link and QR code which can be then shared with people or used however you want.
+
+- Users can sign up to create an account on the app, all of which is handled via Firebase. You can limit sign ups to certain whitelisted domains (this can be done on Firebase as well).
+
+- As [Infura](https://infura.io) no longer provides a free IPFS gateway, you will need to create an account and use your own API keys in order to run your own instance. You will also need to provide a Firebase API key. 
+
 ## 🎬 Demo Video 
-
 
 https://user-images.githubusercontent.com/71545386/197403658-6c77614c-cb20-435b-87ec-9dcf0c918a18.mov
 
 
-##  🔧 Setting up Local Development
+## 🔧 Setting up Local Development
 
 Required: 
 - [Node v17](https://nodejs.org/download/release/latest-v17.x/)  
 - [Yarn](https://classic.yarnpkg.com/en/docs/install/) 
 - [Git](https://git-scm.com/downloads)
 
-Clone the repo and install the node_modules
+Clone the repo and install the necessary dependencies with `npm install`
+
 ```bash
-git clone https://github.com/codeTIT4N/IPFS-upload.git
-cd IPFS-upload
-yarn
+git clone https://github.com/et0and/estuary
+cd estuary
+npm install
 ```
+
 Rename the .env.example to .env and set the following secret variables accordingly:
 1. REACT_APP_INFURA_PROJECT_ID
 2. REACT_APP_INFURA_PROJECT_SECRET
+3. FIREBASE_API
+4. FIREBASE_AUTH_DOMAIN
+5. FIREBASE_PROJECT_ID
+6. FIREBASE_STORAGE_BUCKET
+7. FIREBASE_MSG_SENDER_ID
+8. FIREBASE_APP_ID
 
-You can get these by signing up for the infura IPFS service. It requires a credit card but they won't charge you until you cross the free 5GB limit.
+You can get these by signing up for the infura IPFS service and Firebase respectively. Infura requires a credit card but they won't charge you until you cross the free 5GB limit.
 
 To run the application:
 ```bash
-yarn start
+npm start
 ```
+
 The site is now running at `http://localhost:3000`!
-Open the source code and start editing!
+
+## Credits
+
+Many thanks to [Lokesh Kumar (@codeTIT4N)](https://github.com/codeTIT4N) for the original [IPFS-upload](https://github.com/codeTIT4N/IPFS-upload) repo, for which this is largely based off. 
--- src/App.js
+++ src/App.js
@@ -1,64 +1,87 @@
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import logo from './logo.png';
 import Loader from './Loader';
 import { encode as base64_encode } from 'base-64';
-require('dotenv').config()
-const IPFS = require('ipfs-api');
+import IPFS from 'ipfs-http-client';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword, sendEmailVerification, createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import QRCode from 'qrcode.react';
+require('dotenv').config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 let secrets = process.env.REACT_APP_INFURA_PROJECT_ID + ':' + process.env.REACT_APP_INFURA_PROJECT_SECRET;
 let encodedSecrets = base64_encode(secrets);
-const ipfs = new IPFS({
-  host: 'ipfs.infura.io', port: 5001, protocol: 'https', headers: {
-    Authorization: 'Basic ' + encodedSecrets
-  }
+const ipfs = IPFS.create({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+  headers: {
+    Authorization: 'Basic ' + encodedSecrets,
+  },
 });
 
-function App() {
+function ProtectedApp({ signOut }) {
   const [buf, setBuf] = useState();
-  const [hash, setHash] = useState("");
+  const [hash, setHash] = useState('');
   const [loader, setLoader] = useState(false);
   const [showLinks, setShowLinks] = useState(false);
 
   const captureFile = (event) => {
-    event.stopPropagation()
-    event.preventDefault()
-    const file = event.target.files[0]
-    let reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
-    reader.onloadend = () => convertToBuffer(reader)
+    event.stopPropagation();
+    event.preventDefault();
+    const file = event.target.files[0];
+    let reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => convertToBuffer(reader);
   };
 
   const convertToBuffer = async (reader) => {
-    //file is converted to a buffer to prepare for uploading to IPFS
     const buffer = await Buffer.from(reader.result);
     setBuf(buffer);
   };
+
   const onSubmit = async (event) => {
     event.preventDefault();
     setLoader(true);
-    let ipfsId
-    const buffer = buf
+    let ipfsId;
+    const buffer = buf;
     await ipfs.add(buffer)
       .then((response) => {
-        ipfsId = response[0].hash
-        console.log("Generated IPFS Hash: ", ipfsId)
+        ipfsId = response.path;
+        console.log('Generated IPFS Hash: ', ipfsId);
         setHash(ipfsId);
       }).catch((err) => {
-        console.error(err)
+        console.error(err);
         alert('An error occurred. Please check the console');
-      })
-    if (ipfsId)
-      setShowLinks(true)
-    else
-      setShowLinks(false)
+      });
+    if (ipfsId) {
+      setShowLinks(true);
+    } else {
+      setShowLinks(false);
+    }
     setLoader(false);
-  }
+  };
+
   if (loader) {
     return (
       <Loader />
-    )
+    );
   }
+
   return (
     <div>
       <h1>Upload files to IPFS</h1>
@@ -72,14 +95,119 @@ function App() {
           <div>
             <p>---------------------------------------------------------------------------------------------</p>
             <h6>IPFS Hash: {hash}</h6>
-            <p>Non clickabe Link: https://ipfs.io/ipfs/{hash}</p>
-            <a href={"https://ipfs.io/ipfs/" + hash}>Clickable Link to view file on IPFS</a>
+            <p>Non clickable Link: https://cloudflare-ipfs.com/ipfs/{hash}</p>
+            <a href={"https://cloudflare-ipfs.com/ipfs/" + hash}>Clickable Link to view file on IPFS</a>
+            <div className="mt-4">
+            <h5>QR Code:</h5>
+              <QRCode value={`https://cloudflare-ipfs.com/ipfs/${hash}`} size={250} />
+              </div>
           </div> :
           <p></p>
-
       }
     </div>
   );
 }
 
+function App() {
+  const [user, setUser] = useState(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isSigningUp, setIsSigningUp] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user && user.emailVerified) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  
+    return () => {
+      unsubscribe();
+    };
+  }, []);  
+
+  const signIn = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setErrorMessage("");
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  const allowedDomains = ["gmail.com", "outlook.com"];
+
+  const isAllowedDomain = (email) => {
+    const domain = email.split("@")[1];
+    return allowedDomains.includes(domain);
+  };
+  
+  const signUp = async (e) => {
+    e.preventDefault();
+    if (!isAllowedDomain(email)) {
+      setErrorMessage("This email domain is not allowed for sign up.");
+      return;
+    }
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      await sendEmailVerification(userCredential.user);
+      setErrorMessage("A verification email has been sent. Please check your inbox.");
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  const signOut = async () => {
+    await signOut(auth);
+  };
+
+  if (user) {
+    return <ProtectedApp signOut={signOut} />;
+  } else {
+    return (
+      <div>
+      <img src={logo} alt="Logo" className="mx-auto d-block mb-4" />
+      {isSigningUp ? <h1>Sign Up</h1> : <h1>Sign In</h1>}
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            <Form onSubmit={isSigningUp ? signUp : signIn}>
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </Form.Group>
+              <Button type="submit">{isSigningUp ? "Sign Up" : "Sign In"}</Button>
+            </Form>
+            {errorMessage && <p>{errorMessage}</p>}
+            {isSigningUp ? (
+              <p>
+                Already have an account?{" "}
+                <Button variant="link" onClick={() => setIsSigningUp(false)}>Sign In</Button>
+              </p>
+            ) : (
+              <p>
+                Don't have an account?{" "}
+                <Button variant="link" onClick={() => setIsSigningUp(true)}>Sign Up</Button>
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  }
+}
+
 export default App;
+
+
+
+
